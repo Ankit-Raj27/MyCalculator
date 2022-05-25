2ad016ecaf0f1a2fd2888d30c89441c4b74541fd
Ankit Raj <ankitrajk27@gmail.com>
2022-05-25 19:06:26 +0530

    calculator

---
+++ index.js
@@ -0,0 +1,23 @@
+let input = [];
+
+// Function that takes input
+let buttons = document.querySelectorAll("button");
+Array.from(buttons).forEach((button) => {
+  button.addEventListener("click", (e) => {
+    if (e.target.innerHTML == "=") {
+      input = eval(input);
+      document.querySelector("input").value = input;
+      
+    } else if (e.target.innerHTML == "AC") {
+      input = "";
+      document.querySelector("input").value = input;
+    } else if (e.target.innerHTML == "C") {
+      input = input.substring(0, input.length - 1);
+      document.querySelector("input").value = input;
+    } else {
+      console.log(e.target);
+      input = input + e.target.innerHTML;
+      document.querySelector("input").value = input;
+    }
+  });
+});
